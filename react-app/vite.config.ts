7c8ff86d8f22a22389edfd9923ae086571c25b14
agussantinelli <agustinsantinelli@gmail.com>
2025-09-10 23:39:02 -0300

    dejo comentada la posibilidad de correrlo con tunnelmole

--- react-app/vite.config.ts
+++ react-app/vite.config.ts
@@ -2,6 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()]
+  /*server: {
+    host: true, 
+    allowedHosts: [
+      'fmco9c-ip-181-165-14-167.tunnelmole.net' 
+      // El host para correrlo con Tunnelmole
+      // Tener en cuenta que varia cada vez que se inicia el túnel.
+
+    ]
+  } 
+  */
 })
