6b5c6b3870fa53054bfcb3f63c359133995e2c93
Agustin Santinelli <agustinsantinelli@gmail.com>
2025-10-15 16:15:18 -0300

    Instalacion + Avance

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -100,8 +100,6 @@ const App: React.FC = () => {
         <Route path="/create-event" element={<AuthRoute allowedRoles={['company']}><Layout><CreateEventPage /></Layout></AuthRoute>} />
       </Routes>
       <ChatAssistant />
-      <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet" />
-      <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet" />
     </div>
   );
 };
