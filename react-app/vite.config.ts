3aaf5eabadb222e3d9b0c4487022e8dcd3658e67
MartinUTN <martinratti10@gmail.com>
2025-10-17 11:50:09 -0300

    paquete agregado

--- react-app/vite.config.ts
+++ react-app/vite.config.ts
@@ -5,8 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   test: {
-    environment: 'jsdom', // necesario para simular el DOM en React
-    globals: true, // permite usar expect(), describe(), etc. sin importar
-    setupFiles: './src/setupTests.ts', // archivo de configuración global opcional
+    environment: 'jsdom', 
+    globals: true, 
+    setupFiles: './src/setupTests.ts', 
   },
 })
