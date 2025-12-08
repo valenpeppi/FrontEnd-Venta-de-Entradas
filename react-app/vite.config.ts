07a17f3f613536e86c92e7a2e272da64c4822fac
Agustin Santinelli <agustinsantinelli@gmail.com>
2025-12-08 01:43:26 -0300

    Elimino Configs innecesarios

--- react-app/vite.config.ts
+++ react-app/vite.config.ts
@@ -5,8 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   test: {
-    environment: 'jsdom', 
-    globals: true, 
-    setupFiles: './src/setupTests.ts', 
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './src/setupTests.ts',
+    css: true,
   },
 })
