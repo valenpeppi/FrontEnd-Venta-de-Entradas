0854f211fd6b475dce2785b41e815532d07355bf
Agustin Santinelli <agustinsantinelli@gmail.com>
2025-12-08 19:11:41 -0300

    Mejora Contact

--- react-app/src/pages/support/Contact.tsx
+++ react-app/src/pages/support/Contact.tsx
@@ -55,21 +55,7 @@ const Contact: React.FC = () => {
       title="Contacto"
       subtitle="¿Tienes alguna pregunta o necesitas ayuda? Estamos aquí para asistirte."
     >
-      <div className={styles.contactMethods}>
-        <div className={styles.contactCard}>
-          <FaEnvelope className={styles.contactIcon} />
-          <h3>Correo Electrónico</h3>
-          <p>Te responderemos en un plazo de 24 horas hábiles.</p>
-          <a href="mailto:support@ticketapp.com" className={styles.contactLink}>support@ticketapp.com</a>
-        </div>
 
-        <div className={styles.contactCard}>
-          <FaPhone className={styles.contactIcon} />
-          <h3>Atención Telefónica</h3>
-          <p>Lunes a Viernes, 9:00 a 18:00 hs (ARG)</p>
-          <a href="tel:+543415555555" className={styles.contactLink}>+54 (341) 555-5555</a>
-        </div>
-      </div>
 
       {!isAdmin && (
         <div className={styles.formContainer}>
