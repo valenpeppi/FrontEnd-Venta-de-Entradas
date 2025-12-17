413f258c85bb02a1e5d4c837362ac28afa28a6a1
gianZaba <gianzabaleta6@hotmail.com>
2025-12-16 21:21:00 -0300

    Optimizacion y correccion.

--- src/types/admin.ts
+++ src/types/admin.ts
@@ -26,13 +26,9 @@ export interface AdminEvent {
     } | null;
 }
 
-<<<<<<< HEAD
 export interface DashboardStats {
     salesToday: number;
     ticketsToday: number;
     revenueToday: number;
     pendingEvents: number;
 }
-=======
-
->>>>>>> 2642f6b34117c46e7bdd79439d0745eece75132c
